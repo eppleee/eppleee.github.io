9605792ca775b1f352db5f9d742919e292ad41b2
eppleee <kgiles3@students.towson.edu>
2025-08-11 16:21:44 -0400

    refactor: redo Projects component with mapped cards

--- website/src/pages/Projects.jsx
+++ website/src/pages/Projects.jsx
@@ -1,9 +1,27 @@
+import '../pages/pages.css';
+import Card from '../components/Card.jsx';
+import profilePic from '../assets/pfpPic.jpg';
 
-function Projects(props) {
+function Projects() {
+    const projects = [
+        { title: 'Solitaire', role: 'filer', imgSrc: profilePic, description: 'game' },
+        { title: 'Music Bot', role: 'filer', imgSrc: profilePic, description: 'music' },
+        { title: 'Desktop Simulator', role: 'filer', imgSrc: profilePic, description: 'simulator' }
+    ];
+
     return (
-        <div>
-            <p>Project 1: {props.name}</p>
-        </div>
-    )
+        <section className="project-page">
+
+            <div className="projects">
+                <h1 className="project-text">Projects</h1>
+                <div className="card-grid">
+                    {projects.map((p, index) => (
+                        <Card key={index} {...p} />
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
 }
+
 export default Projects;
