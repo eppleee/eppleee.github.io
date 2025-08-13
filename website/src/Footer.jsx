c2eda606367b657ab94ba82db8299565460ec01d
eppleee <kgiles3@students.towson.edu>
2025-08-13 19:29:35 -0400

    style: rename class in Header component

--- website/src/Footer.jsx
+++ website/src/Footer.jsx
@@ -1,7 +1,7 @@
 
 function Footer() {
     return (
-        <footer className="bg-primary text-primary-foreground p-4 text-center">
+        <footer className="footer text-primary-foreground p-4 text-center" >
             <p>&copy; {new Date().getFullYear()} Kourtney Giles' Website</p>
         </footer>
     );
