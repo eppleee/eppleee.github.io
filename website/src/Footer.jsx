6340d173c154977a956b6a9efe3fceb175c6e6c1
eppleee <kgiles3@students.towson.edu>
2025-08-13 20:16:40 -0400

    Move website files from website/ folder to root for GitHub Pages

--- website/src/Footer.jsx
+++
@@ -1,9 +0,0 @@
-
-function Footer() {
-    return (
-        <footer className="footer text-primary-foreground p-4 text-center" >
-            <p>&copy; {new Date().getFullYear()} Kourtney Giles' Website</p>
-        </footer>
-    );
-}
-export default Footer;
