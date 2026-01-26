ee4786f748043a2948cf98244952e126e912ca2c
eppleee <kgiles3@students.towson.edu>
2026-01-25 22:22:47 -0500

    fix: base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
-  base: '/eppleee.github.io/',
+  base: '/',
   plugins: [
     react(),
     tailwindcss({
